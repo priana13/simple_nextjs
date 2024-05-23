63e0be775ce9adc9bbf4539240ae5ed0c9fd5f0c
priana13 <herian.sap@gmail.com>
2024-05-23 14:08:24 +0700

    Navigasi

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        
+     
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Kita Sedang Belajar NExt
+         
+          <ul className="flex">
+            <li className="mx-2"> <Link href="/">Home</Link> </li>
+            <li className="mx-2"><Link href="/dashboard">Dashboard</Link></li>
+            <li className="mx-2">Settings</li>
+          </ul>
+
         </p>
         </div>
     </main>
