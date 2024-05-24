a46589917a0e32ebf3a805371acc6b53f19ef33f
priana13 <herian.sap@gmail.com>
2024-05-24 11:17:25 +0700

    Loading component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
         
      
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
          
           <ul className="flex">
             <li className="mx-2"> <Link href="/">Home</Link> </li>
@@ -15,7 +14,6 @@ export default function Home() {
             <li className="mx-2">Settings</li>
           </ul>
 
-        </p>
         </div>
     </main>
   )
