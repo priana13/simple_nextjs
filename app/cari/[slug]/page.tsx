bd948f548325b1c8c3fee8e8aec4c65557acff35
priana13 <herian.sap@gmail.com>
2024-05-25 09:42:20 +0700

    get data from API with Fetch

--- app/cari/[slug]/page.tsx
+++ app/cari/[slug]/page.tsx
@@ -1,11 +1,26 @@
 import React from "react"
 
 
-export default function DetailCari({params}: {params: {slug:string}}) {
+
+async function getData(param:string){
+
+  const res = fetch(`https://api.github.com/search/users?q=${param}`)
+
+  return (await res).json();
+}
+
+
+
+export default async function DetailCari({params}: {params: {slug:string}}) {
+
+  const data = await getData(params.slug);
 
   return (
     <div>
         Detail User : {params.slug}
+        <div>
+          {JSON.stringify(data)}
+        </div>
     </div>
   )
 }
